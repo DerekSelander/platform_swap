2531be6976d25271008d9ba9d5d8e6eb047292b0
Derek <dk.selander@gmail.com>
2024-06-03 19:09:36 -0600

    Initial Commit

---
+++ platform_swap/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  platform_swap
+//
+//  Created by Derek Selander on 6/3/24.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
